9d3b86601519b7ba9a9ad4496467f20bdbc01b62
bnaegel <b.naegel@unistra.fr>
2017-01-22 17:23:56 +0100

    Mise à jour P4y

--- TP1/src/image.h
+++ TP1/src/image.h
@@ -9,45 +9,47 @@
 #ifndef image_h
 #define image_h
 
-namespace P4y {
-
-class Image8b {
-    /// classe stockant une image 8 bits (256 valeurs)
+template<class T>
+class Image {
+    /// classe générique codant une image de type T. T doit être précisé lors de l'instanciation.
+    /// Exemple :
+    /// Image<uint8_t> image(10,5);  // instancie une image dont les pixels sont de type uint8_t, de taille 10x5
+    /// Image<float> image(512,512); // instancie une image dont les pixels sont de type float, de taille 512x512
     
 private:
-    int width;
-    int height;
-    int size;
-    unsigned char *data;
+    int dx; /// largeur
+    int dy; /// hauteur
+    int size; /// nombre de pixels de l'image
+    T *data; /// Pointeur vers les valeurs des pixels de l'image. Les pixels sont stockés ligne par ligne.
+             /// Les valeurs des pixels ont le type T, qui est le paramètre de la classe générique.
+             /// Par exemple, dans le cas d'une image 8 bits, le type est Image<uint8_t> et les valeurs ont toutes le type uint8_t
     
 public:
-    /// construit une image de taille (width,height)  (par défaut 0)
-    Image8b(int width=0, int height=0);
-    /// construit une image de taille (width,height) avec le buffer data
-    Image8b(int width, int height, unsigned char *data);
+    /// construit une image de taille (dx,dy) (par défaut 0) avec le buffer data (optionnel)
+    Image(int dx=0, int dy=0, T *data=nullptr);
     
     /// constructeur de copie, permettant d'écrire Image B; Image A(B);
-    Image8b(const Image8b& image8b);
+    Image(const Image& image);
     
     /// opérateur d'affectation permettant d'écrire Image B; Image A=B;
-    Image8b& operator=(const Image8b&);
+    Image& operator=(const Image&);
     
     ///destructeur : instructions pour désallouer les ressources de la classe (ici, le pointeur data)
-    ~Image8b();
-    int getWidth() const;
-    int getHeight() const;
+    ~Image();
+    int getDx() const;
+    int getDy() const;
     int getSize() const;
-    unsigned char *getData();
+    T *getData() const;
     
     /// affiche sur la sortie standard la valeur des pixels de l'image
     void print();
     
     /// surcharge de l'opérateur () permettant d'écrire une valeur à la position (x,y) : A(x,y)=v
-    unsigned char &operator()(const int &x, const int &y);
+    T &operator()(const int &x, const int &y);
     /// surcharge de l'opérateur () permettant de lire une valeur à la position (x,y) : v=A(x,y)
-    unsigned char operator()  (const int &x, const int &y) const;
+    T operator()  (const int &x, const int &y) const;
 };
 
-} // namespace P4y
+#include "image.hpp"
 
 #endif /* image_h */
