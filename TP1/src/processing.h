172aa8465221772e37f7cbd2602177f068ca6d54
jules van assche <j.vanassche@etu.unistra.fr>
2017-02-27 17:23:15 +0100

    ajout fonction masque gaussien, convolution et conversion image double > image uint8

---
+++ TP1/src/processing.h
@@ -0,0 +1,26 @@
+//
+//  treatments.h
+//  tp-P4y
+//
+//
+//
+//
+#ifndef treatments_h
+#define treatments_h
+
+#include <iostream>
+#include <cstdint>
+#include "image.h"
+
+void thresholding(Image<uint8_t> &image, const int value);
+void negation(Image<uint8_t> &image);
+void quantize(Image<uint8_t> &image, int k);
+void resampleOneDimension(int tab[], int factor);
+void resampleOneDimensionBilinear(int tab[], int factor);
+Image<uint8_t> resampleNN(Image<uint8_t> &img, int factor);
+Image<uint8_t> resampleBilinearInterpolation(Image<uint8_t> &img, int factor);
+Image<double> convolve(Image<uint8_t> &img, Image<double> &mask);
+Image<double> gaussianMask(float sigma);
+Image<uint8_t> toUint8(Image<double> &img);
+
+#endif /* treatments_h */
