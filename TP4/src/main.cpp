1a189fdfab27624c5892881aed1eac06d5d82828
jules van assche <j.vanassche@etu.unistra.fr>
2017-03-16 19:09:35 +0100

    ajout des tests image synthéthiques

--- TP4/src/main.cpp
+++ TP4/src/main.cpp
@@ -39,20 +39,43 @@ int main(int argc, const char * argv[]) {
     Image<uint8_t> medianTest(3,3,bufferMedian);
     medianFilter(medianTest,3).print();
     medianTest.print();*/
-    std::vector<Couple> pB = { {10,10},{9,10},{8,10},{7,10},{6,10},{5,10},
+    /*std::vector<Couple> pB = { {10,10},{9,10},{8,10},{7,10},{6,10},{5,10},
                                     {6,5},{7,5},{8,5},{9,5},{10,5},
                                     {5,9},{5,8},{5,7},{5,6},{5,5},
-                                  {10,6},{10,7},{10,8},{10,9} };
+                                  {10,6},{10,7},{10,8},{10,9} };*/
+
+   uint8_t buffer[]={
+     0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,
+     0,0,0,255,255,255,255,0,0,0,
+     0,0,0,255,255,255,255,0,0,0,
+     0,0,0,255,255,255,255,0,0,0,
+     0,0,0,255,255,255,255,0,0,0,
+     0,0,0,255,255,255,255,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0
+   };
+   Image<uint8_t> T(10,10,buffer);
+
+
 
    //Structel b = Structel(pB);
-   Structel b = Structel::disque(2);
+   Structel b = Structel::disque(1);
+   /*
    writePGM(b.internalGradient(E),"./imagesResultat/intGradLenaDisque2.pgm");
    b = Structel::disque(4);
     writePGM(b.internalGradient(E),"./imagesResultat/intGradLenaDisque4.pgm");
   b = Structel::disque(8);
-     writePGM(b.internalGradient(E),"./imagesResultat/intGradLenaDisque8.pgm");
+     writePGM(b.internalGradient(E),"./imagesResultat/intGradLenaDisque8.pgm");*/
 
    //writePGM(a.externalGradient(E),"./../images/externalGradientSoil.pgm");
    //Structel::granulometry(E);
+
+   printf("%s\n","Image originale :");
+   T.print();
+   T = b.closure(T);
+   printf("%s\n","Image modifié :" );
+   T.print();
     return 0;
 }
